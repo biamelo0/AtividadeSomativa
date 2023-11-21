ca7ca86f194e6cd62bb8ecd30bce6429c33b1d46
biamelo0 <132359915+biamelo0@users.noreply.github.com>
2023-11-21 09:35:13 -0300

    Add files via upload

---
+++ AtividadeSomativa/src/main/java/com/AtividadeSomativa/repository/CursoRepository.java
@@ -0,0 +1,9 @@
+package com.AtividadeSomativa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.AtividadeSomativa.entities.Curso;
+
+public interface CursoRepository extends JpaRepository<Curso, Long>{
+
+}
